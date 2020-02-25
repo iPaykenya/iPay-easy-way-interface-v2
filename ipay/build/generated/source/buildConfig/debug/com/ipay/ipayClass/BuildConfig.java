779f9891245754a194afd698f131b7d85a5ffb34
Jude KIkuyu <jude@ipayafrica.com>
2020-02-25 07:41:35 +0300

    change of project gradle version to 3.2

--- ipay/build/generated/source/buildConfig/debug/com/ipay/ipayClass/BuildConfig.java
+++ ipay/build/generated/source/buildConfig/debug/com/ipay/ipayClass/BuildConfig.java
@@ -5,11 +5,6 @@ package com.ipay.ipayClass;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String LIBRARY_PACKAGE_NAME = "com.ipay.ipayClass";
-  /**
-   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
-   */
-  @Deprecated
   public static final String APPLICATION_ID = "com.ipay.ipayClass";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
